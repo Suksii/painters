045b457da950c72ccb1c48d0e16f0d7c90c4d290
unknown <ramovic225@gmail.com>
2024-11-21 23:10:40 +0100

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import "./App.css";
 import MainPage from "./pages/MainPage";
+import {Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div>
-      <MainPage />
+      <Routes>
+        <Route path="/" element={<MainPage />}/>
+        <Route path="/djokaj_djeljos" />
+        <Route path="/gjokaj_agron" />
+        <Route path="/kapllani_edin" />
+        <Route path="/prelvukaj_zake" />
+        <Route path="/rijevac_masanovic_renata" />
+        <Route path="/kolja_berisaj" />
+      </Routes>
     </div>
   );
 }
